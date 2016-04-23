fe08239cea3215909aec39c904878438a94b7b98
Ruifeng Wang <wrf22805656@users.noreply.github.com>
2016-04-22 19:50:13 -0600

    help you grab some information online

---
+++ test.urlib2.py
@@ -0,0 +1,36 @@
+#coding:utf8
+#coding:utf8
+
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+
+url = "http://www.baidu.com"
+
+print ('method 1')
+
+response1 = urllib2.urlopen(url)
+print (response1.getcode())
+print (len(response1.read()))
+
+
+print ('method 2')
+request = urllib2.Request(url)
+request.add_header("user-agent", "Mozilla/5.0")
+response2 = urllib2.urlopen(request)
+print (response2.getcode())
+print (len(response2.read()))
+
+
+print ("method 3")
+import http.cookiejar
+
+cj = http.cookiejar.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+urllib2.install_opener(opener)
+response3 = urllib2.urlopen(url)
+print (response3.getcode())
+print (len(response3.read()))
+
+
